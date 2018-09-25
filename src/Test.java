c283dd831c7fb55bd63a80dd723cf15335f21467
cchherish <1160696359@qq.com>
2018-09-25 16:55:20 +0800

    test git

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import static java.lang.System.out;
+
+/**
+ * Created by Enzo Cotter on 2018/9/25.
+ */
+public class Test {
+    public static void main(String args[]){
+        out.println("i am a boy");
+
+
+    }
+
+}
